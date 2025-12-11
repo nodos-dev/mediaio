e40a9f4175bf2befa6a3065a3f8b39562c74a2c0
atkurtul <atilkurtulmus@gmail.com>
2025-12-11 12:41:26 +0300

    Add debayer node (#4)
    * Add debayer node
    
    * Make the requested changes
    
    * Update version from 2.6.1 to 2.7.0

--- nosMediaIO/Source/MediaIOMain.cpp
+++ nosMediaIO/Source/MediaIOMain.cpp
@@ -32,6 +32,7 @@ enum Nodes : int
 	RGBAToBGR24Buffer,
 	FieldJuggler,
 	SetInterlacedFieldType,
+	Debayer,
 	Count
 };
 
@@ -48,6 +49,7 @@ nosResult RegisterNV12ToRGBA(nosNodeFunctions*);
 nosResult RegisterRGBAToBGR24Buffer(nosNodeFunctions*);
 nosResult RegisterFieldJuggler(nosNodeFunctions*);
 nosResult RegisterSetInterlacedFieldType(nosNodeFunctions*);
+nosResult RegisterDebayer(nosNodeFunctions*);
 
 struct MediaIOPluginFunctions : nos::PluginFunctions
 {
@@ -84,6 +86,7 @@ struct MediaIOPluginFunctions : nos::PluginFunctions
 				GEN_CASE_NODE(RGBAToBGR24Buffer)
 				GEN_CASE_NODE(FieldJuggler)
 				GEN_CASE_NODE(SetInterlacedFieldType)
+				GEN_CASE_NODE(Debayer)
 			}
 		}
 		return NOS_RESULT_SUCCESS;
