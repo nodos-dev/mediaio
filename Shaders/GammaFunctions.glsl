442a21b869556df6f316aba29d1d6d74aecbb06a
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2026-06-23 11:29:39 +0300

    From nodos-1.3: Add option to use gamma curves instead of LUT in RGB-YCbCr conversion nodes (#7)
    Add GammaCurve/UseLUT pins to RGB2YCbCr and YCbCr2RGB nodes plus in-shader
    gamma transfer functions. UseLUT defaults to true to preserve the existing
    LUT-based behavior.
    
    SDR_In_N returns the converted value (vec4(v,1)) rather than the raw input;
    the upstream commit returned vec4(c,1), which discarded the conversion.
    
    (cherry picked from commit 0d9be36845180f2981d9459e91a918992cda7b6d)

---
+++ Shaders/GammaFunctions.glsl
@@ -0,0 +1,68 @@
+// must match Conversion.fbs
+#define GAMMA_CURVE_REC709  0
+#define GAMMA_CURVE_HLG  1
+#define GAMMA_CURVE_ST2084 2
+#define GAMMA_CURVE_SRGB 3
+#define GAMMA_CURVE_IDENTITY 4
+
+vec3 Rec709ToLinear(vec3 c)
+{
+    return mix(c / 4.5, pow((c + 0.099) / 1.099, vec3(1.0 / 0.45)), lessThanEqual(vec3(0.081), c));
+}
+vec3 LinearToRec709(vec3 c)
+{
+    return mix(c * 4.5, pow(c, vec3(0.45)) * 1.099 - 0.099, lessThanEqual(vec3(0.018), c));
+}
+vec3 HLGToLinear(vec3 c)
+{
+    return mix(c * c / 3.0, exp(c / 0.17883277 - 5.61582460179) + 0.02372241, lessThanEqual(vec3(0.5), c));
+}
+vec3 LinearToHLG(vec3 c)
+{
+    return mix(sqrt(c * 3.0), log(c - 0.02372241) * 0.17883277 + 1.00429346, lessThanEqual(vec3(1.0 / 12.0), c));
+}
+
+vec3 ST2084ToLinear(vec3 c)
+{
+    vec3 p = pow(c, vec3(0.01268331));
+    return pow(max(p - 0.8359375, 0.0) / (18.8515625 - 18.6875 * p), vec3(6.27739463));
+}
+
+vec3 LinearToST2084(vec3 c)
+{
+    vec3 p = pow(c, vec3(0.15930175));
+    return pow((0.8359375 + 18.8515625 * p) / (1.0 + 18.6875 * p), vec3(78.84375));
+}
+
+vec3 SRGBToLinear(vec3 c)
+{
+    return mix(c / 12.92, pow((c + 0.055) / 1.055, vec3(2.4)), lessThanEqual(vec3(0.04045), c));
+}
+vec3 LinearToSRGB(vec3 c)
+{
+    return mix(c * 12.92, pow(c, vec3(1.0 / 2.4)) * 1.055 - 0.055, lessThanEqual(vec3(0.0031308), c));
+}
+vec3 IdentityGamma(vec3 c)
+{
+    return c;
+}
+
+vec3 ToLinear(vec3 c, uint curve) {
+    switch (curve) {
+        case GAMMA_CURVE_REC709: return Rec709ToLinear(c);
+        case GAMMA_CURVE_HLG: return HLGToLinear(c);
+        case GAMMA_CURVE_ST2084: return ST2084ToLinear(c);
+        case GAMMA_CURVE_SRGB: return SRGBToLinear(c);
+        default: return c;
+    }
+}
+
+vec3 FromLinear(vec3 c, uint curve) {
+    switch (curve) {
+        case GAMMA_CURVE_REC709: return LinearToRec709(c);
+        case GAMMA_CURVE_HLG: return LinearToHLG(c);
+        case GAMMA_CURVE_ST2084: return LinearToST2084(c);
+        case GAMMA_CURVE_SRGB: return LinearToSRGB(c);
+        default: return c;
+    }
+}
